32a8e0343efc44c939440a3b05eb6e665694738f
13887 <goncalovalacruz@gmail.com>
2023-10-02 15:58:38 +0100

    Ex a)

---
+++ EX1/src/Ex1.java
@@ -0,0 +1,10 @@
+public class Ex1 {
+    public static void main(String[] args) {
+        int x = 100;
+        while(x<=200){
+           System.out.println(x);
+           x++;
+        }
+    }
+
+    }
