381810de891802901705da8f0479191f43f82e74
13887 <goncalovalacruz@gmail.com>
2023-10-02 15:53:38 +0100

    Ex f)

--- EX1/src/Teest2.java
+++ EX1/src/Teest2.java
@@ -1,9 +1,17 @@
+import java.util.Scanner ;
 public class Teest2 {
-    public static void main(String[] args){
-    int a = 100;
-    while(a<=200){
-        System.out.println(a);
-        a++;
-    }
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int x = ler.nextInt();
+        do {
+            if (x % 2 == 0) {
+                x = x / 2;
+            } if(x % 2 != 0) {
+                x = 3 * x + 1;
+
+                System.out.println(x);
+                x++;
+            }
+        }while(x != 1);
     }
 }
